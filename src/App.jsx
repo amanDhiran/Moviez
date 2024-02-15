c12ccf5d8ff173e5d5526901fdccfb9446f0fef3
amanDhiran <amandhiran201@gmail.com>
2024-02-15 19:57:11 +0530

    business logic for hero banner section and custom hook for fetching Api

--- src/App.jsx
+++ src/App.jsx
@@ -17,30 +17,38 @@ function App() {
   const dispatch = useDispatch()
   const {url} = useSelector((state) => state.home
   )
-  console.log(url);
+  
   useEffect(()=>{
-    apiTesting();
+    fetchApiConfig();
   },[])
 
-  const apiTesting = () =>{
-    fetchDataFromApi('/movie/popular')
+  const fetchApiConfig = () =>{
+    fetchDataFromApi('/configuration')
       .then((res) => {
         // console.log(res);
-        dispatch(getApiConfiguration(res))
+        const url = {
+          backdrop: res.images.secure_base_url + "original",
+          poster: res.images.secure_base_url + "original",
+          profile: res.images.secure_base_url + "original",
+        }
+
+        dispatch(getApiConfiguration(url))
       })
   }
 
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element = {<Home/>}/>
-      <Route path="/:mediaType/:id" element = {<Details/>}/>
-      <Route path="/search/:query" element = {<SearchResult/>}/>
-      <Route path="/explore/:mediaType" element = {<Explore/>}/>
-      <Route path="*" element = {<PageNotFound/>}/>
-    </Routes>
+    {/* <Header/> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:mediaType/:id" element={<Details />} />
+        <Route path="/search/:query" element={<SearchResult />} />
+        <Route path="/explore/:mediaType" element={<Explore />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    {/* <Footer/> */}
     </BrowserRouter>
-  )
+  );
 }
 
 export default App
--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,8 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import useFetch from '../hooks/useFetch'
+import { useSelector } from 'react-redux'
 
 function HeroBanner() {
+  const [background, setBackground] = useState("")
+  const [query, setQuery] = useState("")
+  const navigate = useNavigate()
+
+  const {url} = useSelector((state) => state.home)
+
+  const {data, loading} = useFetch("/movie/upcoming")
+
+  useEffect(() => {
+    const bg = url.backdrop + data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path;
+    setBackground(bg)
+  },[data])
+
+  const searchQueryHandler = (e) => {
+    if (e.key === "Enter" && query.length > 0) {
+        navigate(`/search/${query}`)    
+    }
+  }
+
   return (
-    <div>HeroBanner</div>
+    <div>
+      <div>
+        
+      </div>
+      <div>
+        <div>
+          <span>Welcome</span>
+          <span>
+            Millions of movies, TV shows and people to discover.
+            Explore now.
+          </span>
+          <div>
+            <input 
+            type="text"
+            placeholder='Search for a movie or TV show...'
+            onKeyUp={searchQueryHandler}
+            onChange={(e) => setQuery(e.target.value)}
+            
+              />
+            <button>Search</button>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
