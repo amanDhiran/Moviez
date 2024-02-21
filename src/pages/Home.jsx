4dcfe7e2bf0fa5890bfddb796e5c2e8d9d07a487
amanDhiran <amandhiran201@gmail.com>
2024-02-21 21:10:48 +0530

    create top rated component and finish home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import HeroBanner from '../components/HeroBanner'
 import Trending from '../components/Trending'
 import Popular from '../components/Popular'
+import TopRated from '../components/TopRated'
 
 function Home() {
   return (
@@ -9,6 +10,7 @@ function Home() {
         <HeroBanner />
         <Trending />
         <Popular />
+        <TopRated />
     </div>
   )
 }
