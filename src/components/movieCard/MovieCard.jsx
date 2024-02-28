ddf3eac10d3d9c4ec46c5215b92b277aee2aea71
amanDhiran <amandhiran201@gmail.com>
2024-02-28 18:04:38 +0530

    create movie card component

---
+++ src/components/movieCard/MovieCard.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import CircleRating from "../circularRating/CircleRating"
+import Genres from "../Genres";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import './movieCard.css'
+import Img from "../lazyLoadImage/Img";
+import dayjs from "dayjs";
+
+
+function MovieCard({ data, fromSearch, mediaType }) {
+    const { url } = useSelector((state) => state.home);
+    const navigate = useNavigate()
+    const posterUrl = data.poster_path
+        ? url.poster + data.poster_path
+        : PosterFallback;
+  return (
+    <div
+      className="w-[calc(50%-5px)] mb-[25px] cursor-pointer flex-shrink-0 md:w-[calc(25%-15px)] lg:w-[calc(20%-16px)] movieCard"
+      onClick={() => navigate(`/${data.media_type || mediaType}/${data.id}`)}
+    >
+      <div className="posterBlock relative w-full aspect-[1/1.5] bg-cover bg-center mb-[30px] flex items-end justify-between p-[10px] transition-all ease-out duration-[0.5s]">
+        <Img className="w-full h-full object-cover object-center" src={posterUrl} />
+        {!fromSearch && (
+          <>
+            <CircleRating className={" rounded-[50%] p-[2px] w-[40px] h-[40px] relative top-[20px] left-[12px] bg-white flex-shrink-0 md:w-[50px] md:h-[50px]"} rating={data.vote_average.toFixed(1)} />
+            <Genres className = {" m-2 hidden relative md:flex md:flex-wrap md:justify-end"} data={data.genre_ids.slice(0, 2)} />
+          </>
+        )}
+      </div>
+      <div className="text-white flex flex-col">
+        <span className="text-[16px] mb-[10px] leading-6 text-ellipsis line-clamp-1 md:text-xl">{data.title || data.name}</span>
+        <span className=" text-sm opacity-[0.5]">
+          {dayjs(data.release_date).format("MMM D, YYYY")}
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default MovieCard;
--- src/pages/SearchResult.jsx
+++ src/pages/SearchResult.jsx
@@ -1,9 +1,82 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import fetchDataFromApi from "../utils/api";
+import MovieCard from "../components/movieCard/MovieCard";
+import InfiniteScroll from "react-infinite-scroll-component";
+import noResults from "../assets/no-results.png";
+import ContentWrapper from "../components/contentWrappper/ContentWrapper";
 
 function SearchResult() {
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState();
+  const [pageNum, setPageNum] = useState(1);
+  const { query } = useParams();
+
+  function fetchInitialData() {
+    setLoading(true);
+    fetchDataFromApi(`/search/multi?query=${query}&page=${pageNum}`).then(
+      (res) => {
+        setData(res);
+        setPageNum((prev) => prev + 1);
+        setLoading(false);
+      }
+    );
+  }
+
+  function fetchNextPageData() {
+    fetchDataFromApi(`/search/multi?query=${query}&page=${pageNum}`).then(
+      (res) => {
+        if (data?.results) {
+          setData({
+            ...data,
+            results: [...data?.results, ...res.results],
+          });
+        } else {
+          setData(res);
+        }
+        setPageNum((prev) => prev + 1);
+      }
+    );
+  }
+
+  useEffect(() => {
+    setPageNum(1);
+    fetchInitialData();
+  }, [query]);
   return (
-    <div>SearchResult</div>
-  )
+    <div className="searchResultsPage">
+      {loading && <p>loading</p>}
+      {!loading && (
+        <ContentWrapper>
+          {data?.results?.length > 0 ? (
+            <>
+              <div className="pageTitle">
+                {`Search ${
+                  data?.total_results > 1 ? "results" : "result"
+                } of '${query}'`}
+              </div>
+              <InfiniteScroll
+                className="content"
+                dataLength={data?.results?.length || []}
+                next={fetchNextPageData}
+                hasMore={pageNum <= data?.total_pages}
+                loader={loading}
+              >
+                {data?.results.map((item, index) => {
+                  if (item.media_type === "person") return;
+                  return (
+                    <MovieCard key={index} data={item} fromSearch={true} />
+                  );
+                })}
+              </InfiniteScroll>
+            </>
+          ) : (
+            <span className="resultNotFound">Sorry, Results not found!</span>
+          )}
+        </ContentWrapper>
+      )}
+    </div>
+  );
 }
 
-export default SearchResult
+export default SearchResult;
