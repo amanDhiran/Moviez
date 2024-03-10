ea9b8087ccf612d0aca194cf56d9b4443489d2ed
amanDhiran <amandhiran201@gmail.com>
2024-03-10 17:20:52 +0530

    add wishlist icon to movieCard component

--- src/components/movieCard/MovieCard.jsx
+++ src/components/movieCard/MovieCard.jsx
@@ -1,36 +1,70 @@
 import React from "react";
 import CircleRating from "../circularRating/CircleRating"
 import Genres from "../Genres";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import './movieCard.css'
 import Img from "../lazyLoadImage/Img";
 import dayjs from "dayjs";
 import PosterFallback from "../../assets/no-poster.png"
+import { FaHeart } from "react-icons/fa";
+import { addToWatchList } from "../../store/watchListSlice";
 
 
 function MovieCard({ data, fromSearch, mediaType }) {
     const { url } = useSelector((state) => state.home);
     const navigate = useNavigate()
+    const dispatch = useDispatch()
+    const {watchListMovies} = useSelector((state) => state.watchList)
+    let watchListedMovies = watchListMovies.find((movie) => movie.id === data.id)
+              const watchListDisabled = watchListedMovies ? true : false
     const posterUrl = data.poster_path
         ? url.poster + data.poster_path
         : PosterFallback;
+
+        
   return (
     <div
       className="w-[calc(50%-5px)] mb-[25px] cursor-pointer flex-shrink-0 md:w-[calc(25%-15px)] lg:w-[calc(20%-16px)] movieCard"
       onClick={() => navigate(`/${data.media_type || mediaType}/${data.id}`)}
     >
       <div className="posterBlock relative w-full aspect-[1/1.5] bg-cover bg-center mb-[30px] flex items-end justify-between p-[10px] transition-all ease-out duration-[0.5s]">
-        <Img className="w-full h-full object-cover object-center" src={posterUrl} />
+        <Img
+          className="w-full h-full object-cover object-center"
+          src={posterUrl}
+        />
         {!fromSearch && (
           <>
-            <CircleRating textColor={"#04152d"} className={" rounded-[50%] p-[2px] w-[40px] h-[40px] relative top-[20px] left-[12px] bg-white flex-shrink-0 md:w-[50px] md:h-[50px]"} rating={data.vote_average.toFixed(1)} />
-            <Genres className = {" m-2 hidden relative md:flex md:flex-wrap md:justify-end"} data={data.genre_ids.slice(0, 2)} />
+            <CircleRating
+              textColor={"#04152d"}
+              className={
+                " rounded-[50%] p-[2px] w-[40px] h-[40px] relative top-[20px] left-[12px] bg-white flex-shrink-0 md:w-[50px] md:h-[50px]"
+              }
+              rating={data.vote_average.toFixed(1)}
+            />
+            <Genres
+              className={
+                " m-2 hidden relative md:flex md:flex-wrap md:justify-end"
+              }
+              data={data.genre_ids.slice(0, 2)}
+            />
+            <button
+              disabled={watchListDisabled}
+              onClick={(e) => {
+                dispatch(addToWatchList(data));
+                e.stopPropagation();
+              }}
+              className=" disabled:text-red-500 absolute top-3 right-3 rounded-lg text-white text-xl hover:text-red-500 transition-colors ease-in-out duration-300 "
+            >
+              <FaHeart />
+            </button>
           </>
         )}
       </div>
       <div className="text-white flex flex-col">
-        <span className="text-[16px] mb-[10px] leading-6 text-ellipsis line-clamp-1 md:text-xl">{data.title || data.name}</span>
+        <span className="text-[16px] mb-[10px] leading-6 text-ellipsis line-clamp-1 md:text-xl">
+          {data.title || data.name}
+        </span>
         <span className=" text-sm opacity-[0.5]">
           {dayjs(data.release_date).format("MMM D, YYYY")}
         </span>
