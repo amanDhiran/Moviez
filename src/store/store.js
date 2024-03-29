0d7765c9258865214691952713466ed211f34a64
amanDhiran <amandhiran201@gmail.com>
2024-03-29 19:37:15 +0530

    use redux-persist for local storage

--- src/store/store.js
+++ src/store/store.js
@@ -2,11 +2,23 @@ import { configureStore } from '@reduxjs/toolkit'
 import homeSlice from './homeSlice';
 import watchListSlice from './watchListSlice';
 
-const store = configureStore({
-  reducer: {
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' 
+import { combineReducers } from '@reduxjs/toolkit';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const reducer = combineReducers({
     home: homeSlice,
     watchList: watchListSlice
-  },
+})
+const persistedReducer = persistReducer(persistConfig, reducer)
+
+const store = configureStore({
+  reducer: persistedReducer,
 })
 
 export default store;
